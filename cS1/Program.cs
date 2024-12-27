7b64636a8d907c934d05b98b3605e51f68997a44
ldkien <duckienofficial@gmail.com>
2024-12-27 10:35:23 +0700

    first commit

---
+++ cS1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace cS1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Person alex = new();
+            Teacher linda = new();
+            alex.toString();
+            linda.toString();
+
+            Console.WriteLine("---END---");
+
+        }
+
+    }
+
+
+
+
+
+}
---
+++ cS1/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cS1
+{
+    internal class Teacher:Person
+    {
+        public override void toString()
+        {
+            Console.WriteLine("Hello. This is derrived class");
+        }
+    }
+}
