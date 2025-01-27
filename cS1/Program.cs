40708261172a98564f1f2c5bee796e2d9884a32f
ldkien <duckienofficial@gmail.com>
2025-01-27 13:06:02 +0700

    Study LINQ

--- cS1/Program.cs
+++ cS1/Program.cs
@@ -5,7 +5,7 @@
 
         static void Main()
         {
-            var brands = new List<Brand>() 
+            var brands = new List<Brand>()
             {
                 new Brand{ID = 1, Name = "Công ty AAA"},
                 new Brand{ID = 2, Name = "Công ty BBB"},
@@ -14,13 +14,13 @@
 
             var products = new List<Product>()
             {
-                new Product(1, "Bàn trà",    400, new string[] {"Xám", "Xanh"},         2),
-                new Product(2, "Tranh treo", 400, new string[] {"Vàng", "Xanh"},        1),
-                new Product(3, "Đèn trùm",   500, new string[] {"Trắng"},               3),
-                new Product(4, "Bàn học",    200, new string[] {"Trắng", "Xanh"},       1),
-                new Product(5, "Túi da",     300, new string[] {"Đỏ", "Đen", "Vàng"},   2),
-                new Product(6, "Giường ngủ", 500, new string[] {"Trắng"},               2),
-                new Product(7, "Tủ áo",      600, new string[] {"Trắng"},               3),
+                new (1, "Bàn trà",    400, new string[] {"Xám", "Xanh"},         2),
+                new (2, "Tranh treo", 400, new string[] {"Vàng", "Xanh"},        1),
+                new (3, "Đèn trùm",   500, new string[] {"Trắng"},               3),
+                new (4, "Bàn học",    200, new string[] {"Trắng", "Xanh"},       1),
+                new (5, "Túi da",     300, new string[] {"Đỏ", "Đen", "Vàng"},   2),
+                new (6, "Giường ngủ", 500, new string[] {"Trắng"},               2),
+                new (7, "Tủ áo",      600, new string[] {"Trắng"},               3),
             };
 
         }
